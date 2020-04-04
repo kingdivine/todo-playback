067bd1099c405a297e8315ae5e9e3799e312a0f6
kingdivine <divineanamekwe@gmail.com>
2020-04-04 21:44:09 +0100

    Initial commit. Setup React with Webpack and Babel

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    publicPath: "/",
+    filename: "index.js",
+  },
+  devServer: {
+    contentBase: "./dist",
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      hash: true,
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
